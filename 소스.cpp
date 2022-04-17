5e1c741288e69bc489113c1a76ec7a54843b11b1
tlsehdduq <90247013+tlsehdduq@users.noreply.github.com>
2022-04-17 21:57:22 +0900

    save

--- 소스.cpp
+++ 소스.cpp
@@ -1,5 +1,5 @@
 //-----------------------------------------------------------------------------
-//2022.4.6  수 34                    월23수34                          (6주 2일)
+//2022.4.13  수 34                    월23수34                          (6주 2일)
 //
 //  자원을 관리하는 클래스 - STRING - 컨테이너 , 반복자, 알고리즘
 // 
@@ -7,53 +7,55 @@
 // Sequence Container	- 원소의 순서를 임의 지정가능 
 //				- array		유일하게 크기가 컴파일 타임에 결정되어야
 //				- vector	dynamic array (실행 시간에 크기가 바뀌는 배열)
+//				- deque	double ended queue 벡터와 리스트의 중간단계  contiguous가 아니다. 
+//								벡터보다 더 많은 원소를 저장할 수 있다. 
 // 
-// STRING 클래스 이동지원 - 자원을 관리하는 클래스는 이동문법을 지원해야 함
-//		내가 사용하는 것이 아니라 컨테이너 / 알고리즘에서 사용
 // 
-// 클래스의 맴버 변수를 private이 아니라면 ?
-// 
+//	Associative Container
+// Unordered Associative Container
 //				-  중간고사 시험 예정 4/20(수) (7주 2일)
+// 
+//  list 알아오기, 책의 예제 풀어보기,
+// sequence container까지 시험범위 , 
+// 그림공부 정확하게 이해가 되야되고
+// 시험문제는 deque<Dog>.. 클래스를 만들어놓고 컨테이너에 내가만든 객체를 집어넣는  생각해보기 
 //-----------------------------------------------------------------------------
 
 #include<iostream>
-#include<algorithm>
 #include<fstream>
-#include<array>
+#include<algorithm>
 #include<string>
+#include<deque>
 #include "save.h"
 #include "STRING.h"
 
 using namespace std;
 extern bool Check;
 
-
+//[문제] "소스.cpp" 파일의 단어를 deque<string>에 저장하라.
+// 길이오름차순으로 정렬하라
+// 정렬된 결과를 출력하라.
 
 int main()
 {
-
-	// [문제] "소스.cpp"를 읽어 알파벳의 개수를 다음과 같이 출력하라
-	// 대소문자를 구분하진 않는다. 
-	// [a] - 10
-	// [b] - 2
-	// .... 
-	// [z] - 1
-	ifstream in("소스.cpp",ios::binary);
-	char ch;
-	while (in >> ch)
-	{
-
+	ifstream in{ "소스.cpp" };
+	string str;
+	deque<string> dq;
+	while (in >> str) {
+		dq.push_front(str);
 	}
+	for (int i = 0; i < dq.size(); ++i) {
+		cout << dq[i] << '\t';
+	}
+	sort(dq.begin(), dq.end(), [](const string& a, const string& b) {
+		return a.size() < b.size(); });
+
+	cout << endl;
+	cout << endl;
 	
-	
+	for (int i = 0; i < dq.size(); ++i) {
+		cout << dq[i] << endl;
+	}
 
 	save("소스.cpp");
-	
 }
-//1. push_back을 통해 객체를 삽입하기 위해, item 임시 객체를 하나 만듭니다.
-//2. 임시 객체를 복사 생성자를 통해 push_back 함수 내에서 임시 객체를 만들어 냅니다.
-//3. 함수내에 만들어진 임시 객체를 vector 의 끝에 추가합니다.
-//4. 함수를 빠져나온 후, push_back에 삽입하기 위해 만들었던(1번) item 임시 객체를 소멸시킵니다.
-
-// 생성자는 메모리를 생성하는 것이 아닌 생성된 메모리를 활용하는ㄴ내용
-// 소멸자도
