7aec42eea447ca0c2f583800b62a6254c26399db
신동엽 <tlsehdduq98@gmail.com>
2022-04-27 15:03:09 +0900

    2022 04 27

--- STRING.h
+++ STRING.h
@@ -6,8 +6,54 @@
 #pragma once
 
 #include <iostream>
+#include <compare>				//C++20 shapceship operator( three-way comparator )
+
+// 2022 - 04 - 27 STRING이 제공하는 반복자
+class STRING_iterator {
+	char* p;
+
+public:
+	STRING_iterator(char* p) : p{p}{}
+
+	operator-(const STRING_iterator& rhs) const {
+		return p - rhs.p;
+	}
+};
+
+
+// 2022 - 04 - 27 STRING이 제공하는 역방향 반복자.
+class STRING_reverse_iterator {
+	char* p;
+
+public:
+	STRING_reverse_iterator(char* p) : p{ p } {}
+
+	STRING_reverse_iterator& operator++() {
+		--p;
+		return *this;
+	}
+
+	char& operator*() const {
+		return *(p-1);
+	}
+
+	// < , >, == , !=, <=, >=
+	auto operator<=>(const STRING_reverse_iterator& rhs) const = default;
+
+	/*bool operator!=(const STRING_reverse_iterator& rhs) const {
+		std::cout << "자동보다는 사용자 우선" << std::endl;
+		return p != rhs.p;
+	}*/
+};
 
 class STRING {
+
+public:
+	// 2022 - 04 - 27
+	using iterator = STRING_iterator;
+	using reverse_iterator = STRING_reverse_iterator;
+
+private:
 	char* p;							// 확보한 자원 주소
 	size_t num;							// 관리하는 바이트 수
 	int id;								// 생성 시 부여한 id
@@ -27,6 +73,22 @@ public:
 	STRING& operator=(STRING&& other) noexcept;
 	STRING operator+(const STRING& rhs) const;
 
+	// 2022 - 04 - 27 begin()/end() 제공
+	iterator begin() const {
+		return iterator{ p };
+	}
+	iterator end() const {
+		return iterator{ p + num };
+	}
+
+	reverse_iterator rbegin() const {
+		return reverse_iterator{ p + num };
+	}
+
+	reverse_iterator rend() const {
+		return reverse_iterator{ p };
+	}
+
 	// 2022.04.04 추가
 	size_t getNum() const;
 
@@ -34,3 +96,4 @@ public:
 
 	friend std::ostream& operator<<(std::ostream&, const STRING&);
 };
+
--- 소스.cpp
+++ 소스.cpp
@@ -1,61 +1,54 @@
 //-----------------------------------------------------------------------------
-//2022.4.13  수 34                    월23수34                          (6주 2일)
+//2022.4.27  수 34                    월23수34                          (8주 2일)
 //
 //  자원을 관리하는 클래스 - STRING - 컨테이너 , 반복자, 알고리즘
 // 
-//	컨테이너 - 다른 객체를 저장하는 객체
-// Sequence Container	- 원소의 순서를 임의 지정가능 
-//				- array		유일하게 크기가 컴파일 타임에 결정되어야
-//				- vector	dynamic array (실행 시간에 크기가 바뀌는 배열)
-//				- deque	double ended queue 벡터와 리스트의 중간단계  contiguous가 아니다. 
-//								벡터보다 더 많은 원소를 저장할 수 있다. 
-// 
+//	컨테이너 - 다른 객체를 저장하는 객체	
+// Sequence Container
+//				반복자 - Iteratiors are a generalization of pointers 
+//							- that allow a C++ program to work with different data structures 
+//							- in a uniform manner.
+// 반복자란 포인터를 추상화 한것 
+// 자료구조를 일관된 방식으로 접근하기 위해서.
 // 
 //	Associative Container
 // Unordered Associative Container
-//				-  중간고사 시험 예정 4/20(수) (7주 2일)
 // 
-//  list 알아오기, 책의 예제 풀어보기,
-// sequence container까지 시험범위 , 
-// 그림공부 정확하게 이해가 되야되고
-// 시험문제는 deque<Dog>.. 클래스를 만들어놓고 컨테이너에 내가만든 객체를 집어넣는  생각해보기 
+//  C++ 20 concept 을 살펴보자./ (나머지 3개) module, range, coroutine
+// 
+// 오늘 코딩해서 알아볼 내용
+//  - 반복자 종류 판정
+//  - STRING의 반복자를 코딩 - 반복자는 컨테이너가 제공하는 클래스가 되어야
+//  - sort(str.begin(), str.end()); STRING이 sort가 되게 하자
+//		
 //-----------------------------------------------------------------------------
 
 #include<iostream>
-#include<fstream>
 #include<algorithm>
 #include<string>
-#include<deque>
+#include<array>
+#include"STRING.h"
 #include "save.h"
-#include "STRING.h"
 
 using namespace std;
-extern bool Check;
 
-//[문제] "소스.cpp" 파일의 단어를 deque<string>에 저장하라.
-// 길이오름차순으로 정렬하라
-// 정렬된 결과를 출력하라.
+//서로다른 자료형이 들어오니까 템플릿
+template <class Iter>
+void show(Iter)
+{
+	// [문제] Iter의 catergory로 출력하라.
+
+	cout << typeid(Iter::iterator_category).name() << endl;
+
+}
 
 int main()
 {
-	ifstream in{ "소스.cpp" };
-	string str;
-	deque<string> dq;
-	while (in >> str) {
-		dq.push_front(str);
-	}
-	for (int i = 0; i < dq.size(); ++i) {
-		cout << dq[i] << '\t';
-	}
-	sort(dq.begin(), dq.end(), [](const string& a, const string& b) {
-		return a.size() < b.size(); });
-
-	cout << endl;
-	cout << endl;
+	STRING str{ "The quick brown fox jumps over the lazy dog" };
+	// [도전] str을 sort로 정렬할 수 있게 한다.
 	
-	for (int i = 0; i < dq.size(); ++i) {
-		cout << dq[i] << endl;
-	}
+	sort(str.begin(), str.end());
+	
 
 	save("소스.cpp");
 }
